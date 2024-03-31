22f4b5211a7c0700f1dcb8b3ad2cf848c6f9aa7c
MinusMallard <rishavkumar200423@gmail.com>
2024-03-31 20:58:06 +0530

    added NoteScreen

--- app/src/main/java/com/example/letsdo/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/letsdo/ui/navigation/NavGraph.kt
@@ -1,90 +1,122 @@
 package com.example.letsdo.ui.navigation
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MediumTopAppBar
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
+import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.example.letsdo.ui.screen.EntryDestination
 import com.example.letsdo.ui.screen.EntryScreen
 import com.example.letsdo.ui.screen.HomeDestination
 import com.example.letsdo.ui.screen.HomeScreen
+import com.example.letsdo.ui.screen.NoteDestination
+import com.example.letsdo.ui.screen.NoteEntryDestination
+import com.example.letsdo.ui.screen.NoteEntryScree
+import com.example.letsdo.ui.screen.NoteScreen
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MemoLogApp(
-    navController: NavHostController,
-    modifier: Modifier = Modifier
-) {
-    Scaffold(
-        topBar = {
-            MediumTopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text(text = "All Notes",
-                        fontWeight = FontWeight.Medium,
-                        fontSize = 36.sp,
-                    )}
-            )
-        }
-    ) { innerPadding ->
-        NavHost(
-            navController = navController,
-            startDestination = HomeDestination.route,
-            modifier = modifier.padding(innerPadding)
-        ) {
-            composable(route = HomeDestination.route) {
-                HomeScreen(
-                    navigateToEnterEntry = { navController.navigate(EntryDestination.route)},
-                    navigateToUpdateEntry =  {
-                        navController.navigate("${EntryDestination.route}/${it.toString()}")
-                    },
-                )
-            }
-            composable(
-                route = EntryDestination.route,
-            ) {
-                EntryScreen(
-                    navigateBack = { navController.navigate(HomeDestination.route) }
-                )
-            }
-        }
-    }
-}
+
+//@OptIn(ExperimentalMaterial3Api::class)
+//@Composable
+//fun MemoLogApp(
+//    navController: NavHostController,
+//    modifier: Modifier = Modifier
+//) {
+//    Scaffold(
+//        topBar = {
+//            MediumTopAppBar(
+//                colors = TopAppBarDefaults.topAppBarColors(
+//                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+//                    titleContentColor = MaterialTheme.colorScheme.primary,
+//                ),
+//                title = {
+//                    Text(text = "All Notes",
+//                        fontWeight = FontWeight.Medium,
+//                        fontSize = 36.sp,
+//                    )}
+//            )
+//        }
+//    ) { innerPadding ->
+//        NavHost(
+//            navController = navController,
+//            startDestination = HomeDestination.route,
+//            modifier = modifier.padding(innerPadding)
+//        ) {
+//            composable(route = HomeDestination.route) {
+//                HomeScreen(
+//                    navigateToEnterEntry = { navController.navigate(EntryDestination.route)},
+//                    navigateToUpdateEntry =  {
+//                        navController.navigate("${EntryDestination.route}/${it.toString()}")
+//                    },
+//                )
+//            }
+//            composable(
+//                route = EntryDestination.route,
+//            ) {
+//                EntryScreen(
+//                    navigateBack = { navController.navigate(HomeDestination.route) }
+//                )
+//            }
+//        }
+//    }
+//}
+
+data class BottomNavigationItem (
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val route: String
+)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun AppNavHost(
     navController: NavHostController,
     modifier: Modifier = Modifier,
 ) {
-    NavHost(
-        navController = navController,
-        startDestination = HomeDestination.route,
-        modifier = modifier
-    ) {
-        composable(route = HomeDestination.route) {
-            HomeScreen(
-                navigateToEnterEntry = { navController.navigate(EntryDestination.route)},
-                navigateToUpdateEntry =  {
-                    navController.navigate("${EntryDestination.route}/${it.toString()}")
-                },
-            )
-        }
-        composable(
-            route = EntryDestination.route,
+
+        NavHost(
+            navController = navController,
+            startDestination = HomeDestination.route,
+            modifier = modifier
         ) {
-            EntryScreen(navigateBack = {navController.navigate(HomeDestination.route)})
+            composable(route = HomeDestination.route) {
+                HomeScreen(
+                    navigateToEnterEntry = { navController.navigate(EntryDestination.route)},
+                    navController = navController
+                )
+            }
+            composable(
+                route = EntryDestination.route,
+            ) {
+                EntryScreen(navigateBack = {navController.navigate(HomeDestination.route)})
+            }
+            composable(
+                route = NoteDestination.route
+            ) {
+                NoteScreen(
+                    navigateToUpdateNotes = {},
+                    navigateToWriteNote = {navController.navigate(NoteEntryDestination.route)},
+                    modifier = Modifier,
+                    navController = navController
+                )
+            }
+            composable(
+                route = "${NoteEntryDestination.route}/{it}",
+                arguments = listOf(navArgument("it"){type = NavType.IntType})
+            ) { backStackEntry ->
+                val  noteId = backStackEntry.arguments?.getInt("it") ?: -1
+                NoteEntryScree(
+                    navigateBack = {navController.navigate(NoteDestination.route)},
+                    modifier = Modifier,
+                    id = noteId
+                )
+            }
         }
-    }
+
+
 }
+
+
