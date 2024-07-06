21dd7e71bf383d2f078fec12e50c3c608d1f95f4
MinusMallard <rishavkumar200423@gmail.com>
2024-07-06 10:52:51 +0530

    Note.kt

---
+++ app/src/main/java/com/example/letsdo/data/Note.kt
@@ -0,0 +1,12 @@
+package com.example.letsdo.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note")
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    var title: String = "",
+    var description: String = ""
+)
